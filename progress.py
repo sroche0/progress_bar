97f5a224890736f5fc7182a5095861abcdbf47ae
shawn roche <sroche@apperian.com>
2015-10-27 21:58:50 -0400

    prettied up output a bit

--- progress.py
+++ progress.py
@@ -5,7 +5,8 @@ class Bar:
     def __init__(self, total):
         self.last = 0
         self.total = total
-        print ' 0% {} 25% {} 50% {} 75% {} 100%\n['.format('=' * 13, '=' * 14, '=' * 14, '=' * 11),
+        print '{}    Progress    {}'.format('*' * 30, '*' * 30)
+        print ' 0% {} 25% {} 50% {} 75% {} 100%\n['.format('-' * 13, '-' * 14, '-' * 14, '-' * 11),
 
     def update(self, current):
         status = int(float(current) / float(self.total) * 100 * .75)
