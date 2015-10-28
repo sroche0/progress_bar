59a6779ea5751de654984a1fe48b44da5b06402e
shawn roche <sroche@apperian.com>
2015-10-28 16:37:59 -0400

    pretty printing changes

--- progress.py
+++ progress.py
@@ -6,9 +6,9 @@ class Bar:
         self.done = 0
         self.total = total
         print '\n{}{}    Progress    {}{}'.format(' ' * 25, '*' * 5, '*' * 5, ' ' * 25)
-        print '-' * 78
+        print '', '-' * 75
         print '|0% {} 25% {} 50% {} 75% {} 100%|'.format(' ' * 13, ' ' * 14, ' ' * 14, ' ' * 11)
-        print '-' * 78, '\n [',
+        print '', '-' * 75, '\n|',
 
     def update(self, current):
         status = int(float(current) / float(self.total) * 75)
@@ -18,4 +18,4 @@ class Bar:
             self.done = status
 
         if current + 1 == self.total:
-            print '{}{}'.format('#' * (75 - self.done), ']')
+            print '{}{}'.format('#' * (75 - self.done), '|')
