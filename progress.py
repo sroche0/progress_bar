ffcd9b981e73a087af0ad81ac8d51c73f853c044
shawn roche <sroche@apperian.com>
2016-07-01 10:35:03 -0400

    added .gitignore and changed how progressbar prints itsd header and the length of the output

--- progress.py
+++ progress.py
@@ -5,17 +5,15 @@ class Bar:
     def __init__(self, total):
         self.done = 0
         self.total = total
-        print '\n{}{}    Progress    {}{}'.format(' ' * 10, '*' * 20, '*' * 20, ' ' * 10)
         print
-        print ' 0% {} 25% {} 50% {} 75% {} 100%'.format(' ' * 13, ' ' * 14, ' ' * 14, ' ' * 11)
-        print '-' * 77, '\n[',
+        print '0 {} 50 {} 100'.format('-' * 15, '-' * 15)
 
     def update(self, current):
-        status = int(float(current) / float(self.total) * 75)
+        status = int(float(current) / float(self.total) * 40)
         if status != self.done:
             sys.stdout.write('#' * (status - self.done))
             sys.stdout.flush()
             self.done = status
 
         if current + 1 == self.total:
-            print '{}{}'.format('#' * (75 - self.done), ']')
+            print '{}{}'.format('#' * (40 - self.done), ']')
