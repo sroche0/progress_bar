290cf96de41b7938e26eb01411481e5670c040ad
Shawn Roche <sroche0@gmail.com>
2018-05-17 15:31:45 -0400

    updated progress bar to have width be configurable as a param. Changed default behavior from being 40 chars wide, to discovering and using the full width of the terminal if no width is manually specified. Also added a docstring describing how to use the class

--- progress.py
+++ progress.py
@@ -1,19 +1,39 @@
 import sys
+import subprocess
 
 
 class Bar:
-    def __init__(self, total):
+    def __init__(self, total, width=0):
+        """
+        Creates a progress bar object. Width can be manually defined if desired. Default behavior will use the width of
+        the terminal for the progress bar. You instance this outside your loop with the total number of items you expect
+        to process in the loop. Inside the loop you would track how many items you've processed and call update().
+        # of items processed can be cached and passed every X number, you don't need to call update() on every loop
+
+        :param total: Total number of items expected
+        :type total: int
+        :param width: Desired width of the progress bar, minimum of 20 chars long. Default is full screen.
+        :type width: int
+        """
+        if width < 20:
+            rows, columns = subprocess.check_output(['stty', 'size']).split()
+            self.width = int(columns)
+        else:
+            self.width = width
         self.done = 0
         self.total = total
-        print '\n|0 {} 50 {} 100|'.format('-' * 15, '-' * 15)
-        sys.stdout.write('|')
+
+        q = int((self.width - self.width % 4) / 4)
+        leftover = self.width % 4
+        marker = '|'.ljust(q - 1)
+        print('{}{}{}{}{}'.format('0'.ljust(q), marker, marker, marker, '100'.rjust(3 + leftover)))
 
     def update(self, current):
-        status = int(float(current) / float(self.total) * 40)
+        status = int(float(current) / float(self.total) * self.width)
         if status != self.done:
             sys.stdout.write('#' * (status - self.done))
             sys.stdout.flush()
             self.done = status
 
         if current + 1 == self.total:
-            print '{}|\n'.format('#' * (40 - self.done))
+            print('{}\n'.format('#' * (self.width - self.done)))
